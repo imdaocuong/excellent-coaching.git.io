2dc8b2d106cbda8113dc437a16dd76c76230192a
imdaocuong <82821673+imdaocuong@users.noreply.github.com>
2021-07-21 17:47:57 +0700

    Initial commit

---
+++ Test/Test/Test.js
@@ -0,0 +1,32 @@
+// Get the modal
+var hero = document.getElementsByClassName("hero");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("show-login");
+var btn_openLogin = document.querySelector("button#show-login");
+// Get the <span> element that closes the modal
+var close = document.getElementsByClassName("close-btn")[0];
+// btn.onclick = function() {
+// }
+console.log(btn_openLogin);
+// btn_openLogin.addEventListener("click",function(){
+//     hero.style.display = "flex";
+// })
+// console.log(btn_openLogin);
+
+
+// When the user clicks on <span> (x), close the modal
+// close.onclick = function() {
+//   hero.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// hero.onclick = function(event) {
+//   if (event.target == hero) {
+//     hero.style.display = "none";
+//   }
+// }
+    
+
+        
+     
